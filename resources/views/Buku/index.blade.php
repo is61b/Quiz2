4b1c17d877a1c5bd5033d54188df3533629a5a9d
is61b <is61blp3i@gmail.com>
2025-05-30 10:51:20 +0700

    instalasi font awesome (icon)

--- resources/views/Buku/index.blade.php
+++ resources/views/Buku/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">
                     <div class="float-end"><a href="/buku/tambah" class="btn btn-primary btn-sm"><i class="fa-solid fa-user-plus"></i> Tambah Data</a></div>
